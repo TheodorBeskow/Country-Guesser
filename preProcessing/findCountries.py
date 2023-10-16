ab51a1d3edd38c47c64687d5fcc15a449e4c432e
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2023-10-16 09:52:34 +0200

    lables coordinates as in countries

--- preProcessing/findCountries.py
+++ preProcessing/findCountries.py
@@ -1,58 +1,87 @@
 import csv
 import json
 from shapely.geometry import Point, Polygon
-from rtree import index
 import time
 
-# Load the GeoJSON file
-with open(r'Data\world-administrative-boundaries.json', 'r') as f:
-    data = json.load(f)
-
-# Create an R-tree index
-idx = index.Index()
-
-# Create a dictionary to store each country's polygons and colors
-country_polygons = {}
-
-# Loop over each dictionary in the list
-for i, country in enumerate(data):
-    # Extract the coordinates
-    coordinates = country['geo_shape']['geometry']['coordinates']
-
-    # Create polygons for each country and add them to the R-tree index
-    if len(coordinates) > 0 and not isinstance(coordinates[0][0][0], list):
-        for border in coordinates:
-            polygon = Polygon(border)
-            idx.insert(i, polygon.bounds)
-            country_polygons[i] = (country['name'], polygon)
-
-# Load the CSV file
-with open(r'Data\archive\images.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    rows = list(reader)
-
+country_polygons = []
 start_time = time.time()
 
-# Add the country information to each row in the CSV file
-for i, row in enumerate(rows):
-    lat = float(row['lat'])
-    lon = float(row['lng'])
-    point = Point(lon, lat)
+
+# Load the JSON file
+def loadJson():
+    with open(r'Data\world-administrative-boundaries.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def addPolygons(data):
+    # Loop over each dictionary in the list
+    for country in data:
+        before = len(country_polygons)
+        # Extract the coordinates
+        coordinates = country['geo_shape']['geometry']['coordinates']
+
+        # Create polygons for each countr
+        print(len(coordinates))
+        if isinstance(coordinates[0][0][0], list):
+            for polygons in coordinates:
+                for border in polygons:
+                    polygon = Polygon(border)
+                    country_polygons.append([0, country['name'], polygon])
+        else:
+            for border in coordinates:
+                polygon = Polygon(border)
+                country_polygons.append([0, country['name'], polygon])
+        print(country["name"], len(country_polygons)-before)
+
+
+def identifyCountry(row):
+        lat = float(row['lat'])
+        lon = float(row['lng'])
+        point = Point(lon, lat)
+
+        closestCountry = [country_polygons[0][2].exterior.distance(point), 0]
+        
+        for j in range(len(country_polygons)):
+            freq, name, polygon = country_polygons[j]
+            if polygon.contains(point):  # Adjust this threshold as needed
+                row['country'] = name  # Add the country information to the row
+                country_polygons[j][0] += 1
+                return
+            closestCountry = min(closestCountry, [polygon.exterior.distance(point), j])
+        country_polygons[closestCountry[1]][0] += 1
+        row["country"] = closestCountry[1]
+               
+
+def findCountries():   
+    global start_time 
+    # Load the CSV file
+    with open(r'Data\archive\images.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        rows = list(reader)
     
-    # Find the nearest polygon using the R-tree index
-    for j in idx.nearest((point.x, point.y, point.x, point.y), 1):
-        name, polygon = country_polygons[j]
-        row['country'] = name  # Add the country information to the row
-        break
+    for i, row in enumerate(rows):
+        identifyCountry(row)
+        # Print progress every 3 seconds
+        if time.time() - start_time > 3:
+            print(f"Progress: {((i+1)/len(rows))*100}% locations processed")
+            start_time = time.time()
+            country_polygons.sort(key=lambda x:-x[0])
+        
 
-    # Print progress every 3 seconds
-    if time.time() - start_time > 3:
-        print(f"Progress: {((i+1)/len(rows))*100}% locations processed")
-        start_time = time.time()
+    # Write the updated rows back to the CSV file
+    with open(r'Data\archive\images.csv', 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=reader.fieldnames + ['country'])
+        writer.writeheader()
+        writer.writerows(rows)
 
-# Write the updated rows back to the CSV file
-with open(r'Data\archive\images.csv', 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=reader.fieldnames + ['country'])
-    writer.writeheader()
-    writer.writerows(rows)
+
+def main():
+    addPolygons(loadJson())
+    print(len(country_polygons))
+    findCountries()
+
+
+if __name__ == "__main__":
+    main()
 
