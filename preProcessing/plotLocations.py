f91d48a6d50c67c5708050cc87d27934fee1010b
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2023-10-16 20:57:51 +0200

    Ploting the country labled locations

--- preProcessing/plotLocations.py
+++ preProcessing/plotLocations.py
@@ -1,22 +1,55 @@
 import csv
 import matplotlib.pyplot as plt
+import random
+import json
 
-percentage_to_display = 1  
+# Define a function to generate random colors
+def get_random_color():
+    r = lambda: random.randint(0,255)
+    return '#%02X%02X%02X' % (r(),r(),r())
 
+# Load the CSV file
 with open(r'Data\archive\images.csv', 'r') as file:
     reader = csv.DictReader(file)
-    latitudes = []
-    longitudes = []
-    for row in reader:
-        lat = float(row['lat'])
-        lon = float(row['lng'])
-        latitudes.append(lat)
-        longitudes.append(lon)
+    rows = list(reader)
 
-num_points = int(len(latitudes) * percentage_to_display)
+# Create dictionaries for latitudes, longitudes, and colors
+latitudes = {}
+longitudes = {}
+colors = {}
+countries = {}
 
-plt.scatter(longitudes[:num_points], latitudes[:num_points], s=1)
-plt.title('Plot of Coordinates')
+for row in rows:
+    lat = float(row['lat'])
+    lon = float(row['lng'])
+    country = row['country']
+
+    # If the country is not in the dictionaries, add it
+    if country not in latitudes:
+        latitudes[country] = []
+        longitudes[country] = []
+        colors[country] = get_random_color()
+
+    latitudes[country].append(lat)
+    longitudes[country].append(lon)
+    if country in countries: countries[country] += 1 
+    else: countries[country] = 1
+
+sorted_countries = {k: v for k, v in sorted(countries.items(), key=lambda item: -item[1])}
+for k, v in sorted_countries.items():
+    print(k,"has", v,"locations")
+
+percentage_to_display = 1
+
+fig, ax = plt.subplots()
+
+# Plot the points for each country
+for country in latitudes:
+    num_points = int(len(latitudes[country]) * percentage_to_display)
+    ax.scatter(longitudes[country][:num_points], latitudes[country][:num_points], s=1, color=colors[country])
+
+
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
+plt.legend()
 plt.show()
