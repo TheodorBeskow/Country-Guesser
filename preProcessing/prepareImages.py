b9ec60307c366d93cb6cc5e1a3fecd1ce0f1fa1d
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2024-03-16 15:28:34 +0100

    Brazil start model

--- preProcessing/prepareImages.py
+++ preProcessing/prepareImages.py
@@ -8,13 +8,20 @@ from multiprocessing import Pool
 from tqdm import tqdm
 from collections import defaultdict
 import math
+from unidecode import unidecode
+
+def ascii_state_name(state_name):
+    # Transliterate to closest ASCII representation
+    state_name = unidecode(state_name)
+    return state_name
 
 country_amount = defaultdict(int)
-PER_COUNTRY = 3000
+PER_COUNTRY = 1000
 
 def process_image(row):
     
-    if row['country'] != "Denmark" and row['country'] != "Sweden" and row['country'] != "Norway" and row['country'] != "Poland" and row['country'] != "Finland" and row['country'] != "Mexico": return
+    # if row['state'] != "United States of America": return
+    # if row['state'] != "Denmark" and row['state'] != "Sweden" and row['state'] != "Norway" and row['state'] != "Poland" and row['state'] != "Finland" and row['state'] != "Mexico": return
 
     image_files = glob.glob(f"{images_dir_path}/{row['id']}.*")
     if not image_files: return
@@ -34,34 +41,39 @@ def process_image(row):
     output_dir_path = output_test_path if location<train_size+test_size else output_val_path
     if location<train_size: output_dir_path = output_train_path
 
-    amounts = random.uniform(math.floor(PER_COUNTRY/country_amount[row['country']]), math.floor(PER_COUNTRY/country_amount[row['country']]+1))
-    # print(row['country'],  PER_COUNTRY/country_amount[row['country']])
+    amounts = random.uniform(math.floor(PER_COUNTRY/country_amount[row['state']]), math.floor(PER_COUNTRY/country_amount[row['state']]+1))
+    # print(row['state'],  PER_COUNTRY/country_amount[row['state']])
+    for am in range(math.floor(PER_COUNTRY/country_amount[row['state']]) if amounts>PER_COUNTRY/country_amount[row['state']] else math.floor(PER_COUNTRY/country_amount[row['state']]+1)):
+        if am >= 10: break
+        state = ascii_state_name(row['state'])
+        output_image_file_path = f"{output_dir_path}/{state}/{row['id']}Num{am}.jpg"
 
-    for am in range(math.floor(PER_COUNTRY/country_amount[row['country']]) if amounts>PER_COUNTRY/country_amount[row['country']] else math.floor(PER_COUNTRY/country_amount[row['country']]+1)):
-        output_image_file_path = f"{output_dir_path}/{row['country']}/{row['id']}Num{am}.jpg"
         dir_name = os.path.dirname(output_image_file_path)
         if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
+            try:
+                os.makedirs(dir_name)
+            except:
+                pass
         obj.convert(outfile=output_image_file_path)
 
     # Ifall det är väldigt få!
-    # if not os.path.exists(f"{output_train_path}/{row['country']}"):
-    #     os.makedirs(f"{output_train_path}/{row['country']}")
-    # if not os.path.exists(f"{output_test_path}/{row['country']}"):
-    #     os.makedirs(f"{output_test_path}/{row['country']}")
-    # if not os.path.exists(f"{output_val_path}/{row['country']}"):
-    #     os.makedirs(f"{output_val_path}/{row['country']}")
+    # if not os.path.exists(f"{output_train_path}/{row['state']}"):
+    #     os.makedirs(f"{output_train_path}/{row['state']}")
+    # if not os.path.exists(f"{output_test_path}/{row['state']}"):
+    #     os.makedirs(f"{output_test_path}/{row['state']}")
+    # if not os.path.exists(f"{output_val_path}/{row['state']}"):
+    #     os.makedirs(f"{output_val_path}/{row['state']}")
 
 
 # Set the percentage of images to copy
 percentage_to_copy = 1
 
 # Set the paths to your files
-csv_file_path = 'Data/archive/images.csv'
+csv_file_path = 'Data/archive/brazil.csv'
 images_dir_path = 'Data/archive/images'
-output_test_path = 'Data/labledCountries/test'
-output_val_path = 'Data/labledCountries/val'
-output_train_path = 'Data/labledCountries/train'
+output_test_path = 'Data/labledStates/test'
+output_val_path = 'Data/labledStates/val'
+output_train_path = 'Data/labledStates/train'
 
 train_size = 0.8
 val_size = 0.10
@@ -73,7 +85,7 @@ with open(csv_file_path, 'r') as file:
     rows = list(reader)
 
 for index in range(len(rows)):
-    country_amount[rows[index]['country']]+=1
+    country_amount[rows[index]['state']]+=1
 
 # Calculate the number of images to copy
 num_images_to_copy = int(len(rows) * percentage_to_copy)
